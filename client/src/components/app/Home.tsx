75dcdc3d1bacc5cdca54f691ae9b6f007dc85524
Denny Hong <hong961127@gmail.com>
2021-08-18 17:31:07 -0400

    Prefetch treatments data into cache on home load

--- client/src/components/app/Home.tsx
+++ client/src/components/app/Home.tsx
@@ -1,17 +1,23 @@
+// eslint-disable-next-line simple-import-sort/imports
 import { Icon, Stack, Text } from '@chakra-ui/react';
 import { ReactElement } from 'react';
 import { GiFlowerPot } from 'react-icons/gi';
 
+import { usePrefetchTreatments } from '../treatments/hooks/useTreatments';
 import { BackgroundImage } from '../common/BackgroundImage';
 
 export function Home(): ReactElement {
+  usePrefetchTreatments();
+
   return (
     <Stack align="center" justify="center" height="84vh">
       <BackgroundImage />
+
       <Text textAlign="center" fontFamily="Forum, sans-serif" fontSize="6em">
         <Icon m={4} verticalAlign="top" as={GiFlowerPot} />
         Lazy Days Spa
       </Text>
+
       <Text>Hours: limited</Text>
       <Text>Address: nearby</Text>
     </Stack>
