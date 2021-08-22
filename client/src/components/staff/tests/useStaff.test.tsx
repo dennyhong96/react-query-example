8995462ff14dc1bf930e6dd67d37c0e35ebda907
Denny Hong <hong961127@gmail.com>
2021-08-22 09:54:53 -0400

    Test useStaff react query hook

--- client/src/components/staff/tests/useStaff.test.tsx
+++ client/src/components/staff/tests/useStaff.test.tsx
@@ -1,5 +1,18 @@
+import { act, renderHook } from '@testing-library/react-hooks';
+
+import { createQueryClientWrapper } from '../../../test-utils';
 import { useStaff } from '../hooks/useStaff';
 
 test('filter staff', async () => {
-  // the magic happens here
+  const { result, waitFor } = renderHook(() => useStaff(), {
+    wrapper: createQueryClientWrapper(),
+  });
+
+  // Show all staff
+  await waitFor(() => expect(result.current.staff).toHaveLength(4));
+
+  // Set filter to only show facial staff
+  act(() => result.current.setFilter('facial'));
+
+  await waitFor(() => expect(result.current.staff).toHaveLength(3));
 });
